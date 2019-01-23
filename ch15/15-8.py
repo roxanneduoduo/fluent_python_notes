e5dba193fc078625d35d0db8ec81b6c3993e0ea6
emily.li321 <emily.li321@gmail.com>
2019-01-23 19:55:28 +0800

    ch15 modification

--- ch15/15-8.py
+++ ch15/15-8.py
@@ -1,6 +1,7 @@
 import csv
 
 
+"""
 with inplace(csvfilename, 'r', newline='') as (infh, outfh):
     reader = csv.reader(infh)
     writer = csv.writer(outfh)
@@ -8,6 +9,6 @@ with inplace(csvfilename, 'r', newline='') as (infh, outfh):
     for now in reader:
         row += ['new', 'columns']
         writer.writerow(row)
-
+"""
 
 # reference http://www.zopatista.com/python/2013/11/26/inplace-file-rewrite/
